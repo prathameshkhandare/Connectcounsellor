e5365c2e749ec4cf1321351861a3668df80b1dd2
rbhavesh1525 <129406732+rbhavesh1525@users.noreply.github.com>
2024-07-22 21:28:18 +0530

    pic select option remove

--- server/App.js
+++ server/App.js
@@ -10,6 +10,9 @@ const coursesRoute = require('./routes/courses');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 const BlogRoute = require('./routes/blog');
 const authRoute = require('./routes/authRoute');
 const appointmentRoutes = require('./routes/Appointment');
