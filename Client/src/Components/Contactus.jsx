b3906e9cc5ec1a238fe964039fb9ac2cb850bef2
rbhavesh1525 <129406732+rbhavesh1525@users.noreply.github.com>
2024-07-24 19:58:55 +0530

    contact us updated

--- Client/src/Components/Contactus.jsx
+++ Client/src/Components/Contactus.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import '../Components/StyleSheets/Contactus.css'
+import React, { useState } from 'react';
+import '../Components/StyleSheets/Contactus.css';
 import { FaWhatsapp, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
 
 const Contactus = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, phone, message } = formData;
+    const mailtoLink = `mailto:rathodbhaveshpdp@gmail.com?subject=Contact Form Submission&body=Name: ${name}%0D%0AEmail: ${email}%0D%0APhone: ${phone}%0D%0AMessage: ${message}`;
+    window.location.href = mailtoLink;
+  };
+
   return (
     <div className="contact-container">
       <div className="contact-form-container">
         <h3 className='send-msg-heading'>Send Us a Message</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="contact-form-group">
-            <input type="text" id="name" name="name" placeholder="Name" required />
+            <input type="text" id="name" name="name" placeholder="Name" required value={formData.name} onChange={handleChange} />
           </div>
           <div className="contact-form-group">
-            <input type="email" id="email" name="email" placeholder="Email" required />
+            <input type="email" id="email" name="email" placeholder="Email" required value={formData.email} onChange={handleChange} />
           </div>
           <div className="contact-form-group">
-            <input type="tel" id="phone" name="phone" placeholder="Phone" required />
+            <input type="tel" id="phone" name="phone" placeholder="Phone" required value={formData.phone} onChange={handleChange} />
           </div>
           <div className="contact-form-group">
-            <textarea id="message" name="message" placeholder="Your Message" required></textarea>
+            <textarea id="message" name="message" placeholder="Your Message" required value={formData.message} onChange={handleChange}></textarea>
           </div>
           <button type="submit" className='contact-submit-button'>Submit</button>
         </form>
