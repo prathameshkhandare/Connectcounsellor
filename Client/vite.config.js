81f3acc5e8b3748947006916e89d77b129283a60
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2024-09-17 16:43:53 +0530

    viteconfig updated to run on mobile also

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,  // Allows access from external devices
+    port: 5173,  // Use the port you're running the dev server on
+  },
+});
